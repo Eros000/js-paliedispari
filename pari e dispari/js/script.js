822cf876dc4d32fe5ae34363a406fde73f03dbe3
Eros000 <e.ghilardi@hotmail.it>
2023-06-20 17:43:38 +0200

    wrote a random function + summ

--- pari e dispari/js/script.js
+++ pari e dispari/js/script.js
@@ -1,7 +1,24 @@
-const userChoice = prompt('scegli pari o dispari')
+const userChoice = prompt('scegli pari o dispari');
 
-console.log('userChoice', userChoice, typeof userChoice)
+console.log('userChoice', userChoice, typeof userChoice);
 
-const userNumber = prompt('scegli un numero da 1 a 5')
+const userNumber = parseInt(prompt('scegli un numero da 1 a 5'));
 
-console.log('userNumber', userNumber, typeof userNumber)
+console.log('userNumber', userNumber, typeof userNumber);
+
+
+
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+let computerNumber = getRndInteger(1, 5);
+
+console.log('computerNumber', computerNumber, typeof computerNumber)
+
+
+
+const summ = userNumber + computerNumber
+
+console.log('summ', summ, typeof summ)
