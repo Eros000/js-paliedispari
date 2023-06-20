dc22fc4866a2267590832cea83b8839022eddebe
Eros000 <e.ghilardi@hotmail.it>
2023-06-20 17:22:56 +0200

    prompt impostati

--- pari e dispari/js/script.js
+++ pari e dispari/js/script.js
@@ -1,3 +1,7 @@
 const userChoice = prompt('scegli pari o dispari')
 
-console.log()
+console.log('userChoice', userChoice, typeof userChoice)
+
+const userNumber = prompt('scegli un numero da 1 a 5')
+
+console.log('userNumber', userNumber, typeof userNumber)
